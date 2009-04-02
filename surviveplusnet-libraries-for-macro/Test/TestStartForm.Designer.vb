daeb409d29a5ab05b84301aa3aacb7b44d142735
shin-ichi <shin-ichi@surviveplus.net>
2009-04-02 04:27:19 +0000

    ・InputDialog を追加。

--- surviveplusnet-libraries-for-macro/Test/TestStartForm.Designer.vb
+++ surviveplusnet-libraries-for-macro/Test/TestStartForm.Designer.vb
@@ -23,6 +23,7 @@ Partial Class TestStartForm
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button
+        Me.Button2 = New System.Windows.Forms.Button
         Me.SuspendLayout()
         '
         'Button1
@@ -34,17 +35,28 @@ Partial Class TestStartForm
         Me.Button1.Text = "&ListDialog テスト"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'Form1
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 41)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(260, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "&InputDialog テスト"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TestStartForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 263)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
-        Me.Name = "Form1"
+        Me.Name = "TestStartForm"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
 
 End Class
